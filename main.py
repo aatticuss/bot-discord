ef7e81e0b13f60aec376fa0a3a41a8b12f6c8a42
aatticuss <arturbr2004@gmail.com>
2024-08-23 22:03:58 -0300

    Primeiros comandos

--- main.py
+++ main.py
@@ -11,14 +11,34 @@ client = discord.Client(intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Me chamo bot {client.user}')
+    print(f'Me chamo bot {bot.user}')
     # await asyncio.sleep(600)
 
-@bot.command()
+with open('token.txt', 'r') as file:
+    token = file.read()
+
+# Comandos
+@bot.command(aliases = ["ola", "eai", "oii", "oie"])
 async def oi(ctx):
     await ctx.send(f"Oi, {ctx.author.mention}, tudo bem?")
 
-with open('file', 'r') as file:
-    token = file.read()
+@bot.command(name = "h")
+async def help(ctx):
+    # Abre uma mensagem embed
+    help_msg = discord.Embed(title="Guia de comandos", description="Abaixo está uma lista com todos os comandos do GPCão.", color=discord.Color.darker_grey())
+    help_msg.add_field(name="espaçamento", value="\u200b", inline=True)
+    # fields são campos de texto
+    help_msg.set_author(name = ctx.author.display_name, icon_url=ctx.author.avatar)
+    help_msg.set_thumbnail(url=ctx.guild.icon)  
+    help_msg.add_field(name="!oi", value="Cumprimenta o usuário.", inline=False)
+    help_msg.add_field(name="!daily", value="Gera um desafio de código diário. ", inline=False)
+    help_msg.add_field(name="!reroll", value="Redefine o desafio gerado. Tome cuidado, você só pode realizar essa ação 3 vezes por dia.", inline=False)
+    help_msg.add_field(name="espaçamento", value="\u200b", inline=True)
+    help_msg.set_footer(text="Fornecido por {}".format(bot.user), icon_url=bot.user.avatar)
+    await ctx.send(embed=help_msg)
 
-bot.run('token')
+@bot.command(name="abt")
+async def about(ctx):
+    await ctx.send(f"Olá, me chamo {bot.user}. Minha função é gerar diariamente um desafio de programação para testar seus conhecimentos e garantir que você não fique um dia sequer sem praticar.\nDigite !help para checar meus comandos!")
+
+bot.run(token)    
